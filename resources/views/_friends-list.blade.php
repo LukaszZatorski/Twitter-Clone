bd6d003812c2ecb41026818218a3b7befab8d248
Zatorski <lukasz.zatorskii@gmail.com>
2020-06-19 12:48:12 +0000

    Make friends list a dynamic list

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,52 +1,30 @@
-<ul class="list-unstyled position-fixed p-2 bg-light border rounded-lg">
-    <li class="mb-3">
-        <a
-            href="#"
-            class="h5 text-decoration-none text-dark"
-        >Lorem, ipsum.</a></li>
-    <li class="mb-3">
-        <a
-            href="#"
-            class="h5 text-decoration-none text-dark"
-        >Delectus, nostrum.</a></li>
-    <li class="mb-3">
-        <a
-            href="#"
-            class="h5 text-decoration-none text-dark"
-        >Nulla, a!</a></li>
-    <li class="mb-3">
-        <a
-            href="#"
-            class="h5 text-decoration-none text-dark"
-        >Nisi, magni?</a></li>
-    <li class="mb-3">
-        <a
-            href="#"
-            class="h5 text-decoration-none text-dark"
-        >Alias, facere!</a></li>
-    <li class="mb-3">
-        <a
-            href="#"
-            class="h5 text-decoration-none text-dark"
-        >Fugit, commodi.</a></li>
-    <li class="mb-3">
-        <a
-            href="#"
-            class="h5 text-decoration-none text-dark"
-        >Neque, similique.</a></li>
-    <li class="mb-3">
-        <a
-            href="#"
-            class="h5 text-decoration-none text-dark"
-        >Molestiae, obcaecati.</a></li>
-    <li class="mb-3">
-        <a
-            href="#"
-            class="h5 text-decoration-none text-dark"
-        >Quisquam, deserunt?</a></li>
-    <li class="mb-3">
-        <a
-            href="#"
-            class="h5 text-decoration-none text-dark"
-        >Tempore, ipsum?</a></li>
-</ul>
+<div class="position-fixed col-2 p-2 bg-light border rounded-lg">
+    <h4 class="d-flex justify-content-center mb-4">Following</h4>
+    <ul class="list-unstyled">
+        @forelse (current_user()->follows as $user)
+        <li class="d-flex align-items-center {{$loop->last ? '' : 'mb-2'}}">
+            <a
+                href="{{route('profile.show', $user->username)}}"
+                class="mr-2"
+            >
+                <img
+                    src="https://avatars.dicebear.com/api/male/{{$user->username}}.svg?b=%2310a6cb&w=40&h=40"
+                    alt=""
+                    class="rounded-circle"
+                >
+            </a>
+            <a
+                href="{{route('profile.show', $user->username)}}"
+                class="text-decoration-none text-dark"
+            >
+                <h6 class="font-weight-bold">{{ $user->name }}</h5>
+            </a>
+        </li>
+        @empty
+        <li class="d-flex justify-content-center">No friends yet!</li>
+        @endforelse
+        <li class="mb-3">
+            <a href=""></a>
+        </li>
+    </ul>
+</div>
--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -1,13 +1,13 @@
 <x-master title="{{isset($title) ? $title . ' / Tweets' : 'Tweets' }}">
     <div class="container">
         <div class="row">
-            <div class="col d-flex mt-4">
+            <div class="col mt-4">
                 @include('_sidebar')
             </div>
             <div class="col-8">
                 {{ $slot }}
             </div>
-            <div class="col d-flex mt-4">
+            <div class="col-2 mt-4">
                 @include('_friends-list')
             </div>
         </div>
